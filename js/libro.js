d371f997355f4ee814e79ee61aaf3f8a146b6979
Nicolás Carro <141870810+riversideecoding@users.noreply.github.com>
2025-03-25 14:31:09 -0300

    LISTO

--- js/libro.js
+++ js/libro.js
@@ -1,7 +1,7 @@
-function obtenerLibros() {
+async function obtenerLibros() {
 
     try {
-        const respuesta=await fech ("../backend/routes/api.php");
+        const respuesta = await fetch ("../backend/routes/api.php");
         const libros=respuesta.json();
         const contenedor=document.getElementById("contenedor-libros");
         contenedor.innerHTML=mostrarLibros(libros);   
@@ -12,13 +12,13 @@ function obtenerLibros() {
 }
 function mostrarLibros(libros) {
     let contenido="";
-    libros.forEach(libro=>{
-        contenido +='<h4>${'libro.titulo'}</h4>';
-        contenido +='<h4>${'libro.ID'}</h4>';
-        contenido +='<h4>${'libro.Titulo'}</h4>';
-        contenido +='<h4>${'libro.Autor'}</h4>';
-        contenido +='<h4>${'libro.Publicacion'}</h4>';
-        contenido +='<h4>${'libro.Disponible'}</h4>';
+    libros.array.forEach(libro => {
+        contenido +='<h4>${libro.id libro}</h4>'
+        contenido +='<h4>${libro.ID}</h4>';
+        contenido +='<h4>${libro.Titulo}</h4>';
+        contenido +='<h4>${libro.Autor}</h4>';
+        contenido +='<h4>${libro.Publicacion}</h4>';
+        contenido +='<h4>${libro.Disponible}</h4>';
     });
 }
     obtenerLibros();
