a5f8296ff137b7314ae91897937299838e0eccf5
Nicolás Carro <141870810+riversideecoding@users.noreply.github.com>
2025-03-25 13:37:39 -0300

    New Stuff

--- js/libro.js
+++ js/libro.js
@@ -3,6 +3,8 @@ function obtenerLibros() {
     try {
         const respuesta=await fech ("../backend/routes/api.php");
         const libros=respuesta.json();
+        const contenedor=document.getElementById("contenedor-libros");
+        contenedor.innerHTML=mostrarLibros(libros);
 
     }   
 
