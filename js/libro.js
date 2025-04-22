69e6a1dd517dbc47fc7dfc879f66084e9a917f4d
Nicolás Carro <nicocarro.07@gmail.com>
2025-04-22 14:22:32 -0300

    nmn

--- js/libro.js
+++ js/libro.js
@@ -13,29 +13,18 @@ function mostrarLibros(libros) {
     let contenido = "";
     libros.forEach(libro => {
         let available = libro.disponible === 1 ? "Si" : "No";
-        contenido += "<tr>"
+        contenido += "<tr id='libro-" + libro.id_libro + "'>";
         contenido += "<td>" + libro.id_libro + "</td>";
         contenido += "<td>" + libro.titulo + "</td>";
         contenido += "<td>" + libro.autor + "</td>";
         contenido += "<td>" + libro.anio_publicacion + "</td>";
         contenido += "<td>" + available + "</td>";
         contenido += "<td><button class='btn-eliminar' onclick='eliminarLibro(" + libro.id_libro + ")'>Eliminar</button></td>";
-        contenido += "</tr>"
-        console.log(libro);
+        contenido += "</tr>";
     });
 
     const table = document.getElementById("body-table");
     table.innerHTML = contenido;
-
-}
-
-async function eliminarLibro(id_libro) {
-    //Selects the row that's gonna be deleted by it's ID
-    const row = document.getElementById("libro-" + id_libro);
-    if (row) { 
-        row.remove(); //Removes row
-        console.log("Libro con ID " + id_libro + " ha sido eliminado.")
-    }
 }
 
 obtenerLibros();
